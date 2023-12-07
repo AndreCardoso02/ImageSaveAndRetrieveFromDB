27f76ba812a9d83e1c737da9b57f4824dcbb77a3
AndreCardoso02 <andremirandacardoso02@gmail.com>
2023-12-07 06:46:03 +0100

    Criando a logica de carregar imagem na base de dados e no servidor

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -10,33 +10,86 @@ using Microsoft.Extensions.Logging;
 
 namespace ImageSaveAndRetrieveFromDB.Controllers
 {
-    [Route("admin/product")]
+    [Route("[controller]/[action]")]
     public class ProductController : Controller
     {
         private readonly ILogger<ProductController> _logger;
         private readonly ApplicationDbContext _context;
+        private IWebHostEnvironment _environment;
 
-        public ProductController(ILogger<ProductController> logger, ApplicationDbContext context)
+        public ProductController(ILogger<ProductController> logger, ApplicationDbContext context, IWebHostEnvironment environment)
         {
             _logger = logger;
             _context = context;
+            _environment = environment;
         }
 
+        [Route("/Products/")]
+        [Route("~/Products/Index")]
         public async Task<IActionResult> Index()
         {
             var products = await _context.Products.ToListAsync();
             return View(products);
         }
 
+        [Route("/Product/Add")]
         public IActionResult Create()
         {
             return View();
         }
 
+        [HttpPost]
+        [Route("/Product/Add")]
+        public IActionResult Create([Bind("Name,Price,ImageFile")] ProductCreateViewModel model)
+        {
+            string filename = "~/";
+
+            if (model.ImageFile == null)
+            {
+                ModelState.AddModelError("ImageFile", "Image not uploaded");
+                return View(model);
+            }
+
+            if (ModelState.IsValid)
+            {
+                // Salvar na pasta Raiz
+                string uploadedFolder = Path.Combine(_environment.WebRootPath, "images");
+                filename = Guid.NewGuid().ToString() + "_" + model.ImageFile.FileName;
+                string filepath = Path.Combine(uploadedFolder, filename);
+                model.ImageFile.CopyTo(new FileStream(filepath, FileMode.CreateNew));
+
+                // Salvar na BD
+
+                Product p = new Product
+                {
+                    Name = model.Name,
+                    Price = model.Price,
+                    Image = ConvertFileToByte(model.ImageFile)
+                };
+
+                _context.Products.Add(p);
+                _context.SaveChanges();
+                ViewBag.Success = "Record Added";
+                return View();
+            }
+            ModelState.AddModelError("ImageFile", "Error adding file");
+            return View(model);
+        }
+
+        [Route("Product/Error")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View("Error!");
+        }
+
+        private byte[] ConvertFileToByte(IFormFile imagem)
+        {
+            using (var ms = new MemoryStream())
+            {
+                imagem.OpenReadStream().CopyTo(ms);
+                return ms.ToArray();
+            }
         }
     }
 }
--- Models/ProductCreateViewModel.cs
+++ Models/ProductCreateViewModel.cs
@@ -1,18 +1,14 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ImageSaveAndRetrieveFromDB.Models
 {
-    public class Product
+    public class ProductCreateViewModel
     {
-        [Key]
-        public int Id { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
-        public byte[] Image { get; set; }
+        public IFormFile ImageFile { get; set; }
     }
 }
